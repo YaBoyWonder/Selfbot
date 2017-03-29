d38db935ec27c7e4dd62c0143e155df0a65e507f
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-29 05:07:01 -0400

    Update self.js

--- code/self.js
+++ code/self.js
@@ -52,25 +52,7 @@ if (message.content.startsWith(prefix + 'avatar')) {
 	}
 
 
-if(message.content.startsWith(prefix + 'quote')) {
-	  if(message.author.id !== "138431969418543104") return;
-  const [replyTo, ...replyText] = args;
-    message.channel.fetchMessages({limit: 1, around: replyTo})
-    .then(messages=> {
-      const replyToMessage = messages.first();
-      message.delete();
-      message.channel.sendMessage(replyText.join(" "), {embed: {
-        color: 3447003,
-        author: {
-          name: `${replyToMessage.author.username}`,
-          icon_url: replyToMessage.author.avatarURL
-        },
-        description: replyToMessage.content
-      }})
 
-      .then(() => message.delete());
-    }).catch(console.error);
-}
 
 
 
@@ -98,21 +80,6 @@ if(message.content.startsWith(prefix + 'roles')) {
 	}});
 	};
 
-https://discord.gg/r7Y6hDc
-
-if(message.content.startsWith(prefix + 'ex3invite')) {
-	if(message.author.id !== "138431969418543104") return;
-	message.channel.sendMessage("", {embed: {
-	color: 0xF7B85D,
-	title: 'Invitation for the chat',
-	url: 'https://discord.gg/r7Y6hDc',
-	timestamp: new Date(),
-	footer: {
-		icon_url: client.user.avatarURL,
-		text: '©BB-8'
-	}
-}});
-};
 
 
 if(message.content.startsWith(prefix + 'stats')) {
@@ -129,43 +96,7 @@ if(message.author.id !== "138431969418543104") return;
 message.channel.sendMessage(`**Discord.js** :: v${Discord.version}`);
 }
 
-if(message.content.startsWith(prefix + 'evall'))  {
-	if(message.author.id !== "138431969418543104") return;
-    let input = `:inbox_tray:**Input:**\n\`\`\`js\n${args.join(' ')}\n\`\`\``;
-    return Promise.all([
-        new Promise((resolve, reject) => {
-            let ev;
 
-            try {
-                ev = eval(args.join(' '));
-
-                if(ev && typeof ev.then == 'function' && typeof ev.catch == 'function')   {
-                    ev.then(resolve).catch(reject);
-                    return;
-                }
-                resolve(ev);
-
-            }   catch(err)    {
-                reject(err);
-            }
-        }),
-    ]).then(resolutions => {
-        let out;
-        const res = resolutions[0];
-        if(typeof res == 'object' && typeof res != 'string')  {
-            out = require('util').inspect(res);
-            if(typeof out == 'string' && out.length > 1900)   {
-                out = res.toString();
-            }
-        }   else {
-            out = res;
-        }
-
-        return message.edit(`${input}:white_check_mark:**Output:**\n\`\`\`js\n${out}\n\`\`\``);
-    }).catch(err => {
-        return message.edit(`${input}:outbox_tray:**Error:**\n\`\`\`js\n${err.message || err}\n\`\`\``);
-    });
-}
 
 
 });
